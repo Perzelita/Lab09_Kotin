019149f084bf15b44c89b9bf29a444a6e5b77f29
Perzelita <33858248+Perzelita@users.noreply.github.com>
2018-11-26 01:26:36 +0700

    Initial commit

---
+++ app/src/main/java/th/ac/kku/cs/lab09_kotin/MainActivity.kt
@@ -0,0 +1,30 @@
+package th.ac.kku.cs.lab09_kotin
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import th.ac.kku.cs.lab09_kotin.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        val KEY = "keydatastudent";
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnsend.setOnClickListener{
+            val i = Intent(this,SecondActivity::class.java)
+
+            val dataStudent = Student(
+                etID.text.toString(),
+                etfirst.text.toString(),
+                etLast.text.toString()
+
+            )
+            i.putExtra(KEY,dataStudent)
+            startActivity(i)
+        }
+    }
+}
